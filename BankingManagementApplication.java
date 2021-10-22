6a5a5b79c34ae00c3ec4f605ea9a462230d7eca6
mishradibyesh <dibyeshmishra207@gmail.com>
2021-10-22 09:13:40 +0530

    updated code for saving accounts

--- BankingManagementApplication.java
+++ BankingManagementApplication.java
@@ -6,11 +6,18 @@ public class BankingManagementApplication {
 		
 		System.out.println("welcome to banking management application");
 		SavingsAcoount saver1 = new SavingsAcoount();
-		saver1.getSavingBalance(2000);
-		saver1.calculateMonthlyInterest();    // calculating saving balance for saver 1
+		saver1.setSavingBalance(2000);
+		saver1.calculateMonthlyInterest(4);    // calculating saving balance for saver 2
 		SavingsAcoount saver2 = new SavingsAcoount();
-		saver2.getSavingBalance(3000);
-		saver2.calculateMonthlyInterest();   // calculating saving balance for saver 2
+		saver2.setSavingBalance(3000);
+		saver2.calculateMonthlyInterest(4);   // calculating saving balance for saver 2
+		
+		System.out.println("after changing interest rate");
+		 // calculating by changing interest rate
+		
+		saver1.calculateMonthlyInterest(5);    // calculating saving balance for saver 1
+		saver2.calculateMonthlyInterest(5);     // calculating saving balance for saver 1
+
 	}
 
 }
--- SavingsAcoount.java
+++ SavingsAcoount.java
@@ -4,17 +4,17 @@ public class SavingsAcoount {
 static  double annulInterestRate;     // to store annul interest rate
 private double savingBalance;          // to store amount the saver currently having 
 
-public void calculateMonthlyInterest() {    // calculating monthly  interest
-	SavingsAcoount saver = new SavingsAcoount();
-	annulInterestRate =  saver.modifyInterestRate(4);     //setting interest rate by calling method interest rate 
-	savingBalance = savingBalance * annulInterestRate ; // calculating saving balace by interest provides
+public void calculateMonthlyInterest(double annulInterestRate) {    // calculating monthly  interest
+	annulInterestRate = modifyInterestRate(annulInterestRate);
+	savingBalance = savingBalance * annulInterestRate ; // calculating saving balance by interest provides
     System.out.println("the new balace is now $"+savingBalance);
 }
 public static double modifyInterestRate(double annulInterestRate ) {
-	annulInterestRate = annulInterestRate ;
-			return annulInterestRate;
+	
+	return annulInterestRate;
 }
-public void getSavingBalance(double savingBalance) {   // setting saving balance private variable
+
+public void setSavingBalance(double savingBalance) {   // setting saving balance private variable
 	this.savingBalance = savingBalance;
 }
 
