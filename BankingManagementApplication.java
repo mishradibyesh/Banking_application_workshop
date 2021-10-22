32944a4ce38ad8610b6309586efa0b8c7cacbd92
mishradibyesh <dibyeshmishra207@gmail.com>
2021-10-22 07:49:38 +0530

    welcome message

---
+++ BankingManagementApplication.java
@@ -0,0 +1,11 @@
+package com.bl.bankingmanagementapplication;
+
+public class BankingManagementApplication {
+
+	public static void main(String[] args) {
+		
+		System.out.println("welcome to banking management application");
+
+	}
+
+}
